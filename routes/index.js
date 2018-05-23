1e39605e18a85c24b678257fbcf74f23752b7040
KJRi <kjrkjri@outlook.com>
2018-05-24 00:18:18 +0800

    完成后台

--- routes/index.js
+++ routes/index.js
@@ -3,4 +3,9 @@ module.exports = (app) => {
   app.use('/api', require('./users')) // 在所有users路由前加/api
   app.use('/info', require('./userinfos'))
   app.use('/good', require('./goods'))
+  app.use('/car', require('./cars'))
+  app.use('/order', require('./orders'))
+  app.use('/fav', require('./favs'))
+  app.use('/birth', require('./births'))
+  app.use('/address', require('./addresses'))
 }
